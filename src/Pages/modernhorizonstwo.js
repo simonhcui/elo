0df7982c659d93f1c4eed820edc79ea1eec8f833
unknown <simonhcui@gmail.com>
2023-09-01 00:33:00 -0700

    mh2 glicko

--- src/Pages/modernhorizonstwo.js
+++ src/Pages/modernhorizonstwo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { TrophiesTable } from "../Components/ModernHorizonsTwo/MH2TrophiesTable";
-import { WinrateTable } from "../Components/ModernHorizonsTwo/MH2WinrateTable";
+//import { TrophiesTable } from "../Components/ModernHorizonsTwo/MH2TrophiesTable";
+//import { WinrateTable } from "../Components/ModernHorizonsTwo/MH2WinrateTable";
 import { Table } from "../Components/ModernHorizonsTwo/MH2ArchetypeTable";
 import { WinrateEloTable } from "../Components/ModernHorizonsTwo/WinrateEloTable";
 
@@ -9,10 +9,10 @@ const ModernHorizonsTwo = () => {
   return (
     <>
       <h1 style={{ textAlign: "center" }}>
-        Modern Horizons 2 Stats (17 Drafts Total)
+        Modern Horizons 2 Stats (16 Drafts Total)
       </h1>
       <h2 style={{ textAlign: "center" }}>
-        ELO and Winrates (Min 4 drafts). Last Updated 5/13/23
+        ELO and Winrates (Min 4 drafts). Last Updated 8/31/23
       </h2>
       <h2 style={{ textAlign: "center" }}></h2>
       <div>
