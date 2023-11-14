6f070d9d3208ae1082b7d69d7283712f2b967a34
unknown <simonhcui@gmail.com>
2023-11-13 17:10:29 -0800

    11/9

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -9,7 +9,7 @@ const Home = () => {
       <h1 style={{ textAlign: "center" }}>Simon LGS Stats</h1>
       <h2 style={{ textAlign: "center" }}>Season 5 (9/30/23 - 12/28/23)</h2>
       <h2 style={{ textAlign: "center" }}>
-        ELO and Winrates. Last Updated 10/28/23 (Min 4 Drafts)
+        ELO and Winrates. Last Updated 11/9/23 (Min 4 Drafts)
         {/* <p>Updated under Season 4, Season 5 just started</p> */}
       </h2>
       <div>
