548df7398a615720541d1ce17d0708f3d7198b07
Simon Cui <scui@asr.ocgov.com>
2024-04-03 15:02:48 -0700

    temp turned off 2x2 archetype trophies

--- src/Components/DoubleMasters/2x2ArchetypeColumns.js
+++ src/Components/DoubleMasters/2x2ArchetypeColumns.js
@@ -15,8 +15,8 @@ export const COLUMNS = [
     Header: "Number of Drafts",
     accessor: "num_drafts",
   },
-  {
-    Header: "Trophies",
-    assessor: "num_trophies",
-  },
+  // {
+  //   Header: "Trophies",
+  //   assessor: "num_trophies",
+  // },
 ];
