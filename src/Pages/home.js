3d294f5291ef3055c04ffbe6db1f8b82cb7690b0
Simon Cui <scui@asr.ocgov.com>
2024-04-23 15:47:45 -0700

    first s7 uodate:

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -9,12 +9,12 @@ const Home = () => {
       <h1 style={{ textAlign: "center" }}>Simon LGS Stats</h1>
       <h2 style={{ textAlign: "center" }}>Season 7 (4/4/24 - 6/20/24 (Tentative))</h2>
       <h2 style={{ textAlign: "center" }}>
-        {/* ELO and Winrates. Last Updated 3/14/24 (Min 4 Drafts) */}
-        <p>Updated under Season 6 Page, Season 7 starting soon!</p>
+        ELO and Winrates. Last Updated 4/17/24 (Min 4 Drafts)
+        {/* <p>Updated under Season 6 Page, Season 7 starting soon!</p> */}
       </h2>
       <div>
         <View style={styles.container}>
-          {/* <WinrateEloTable /> */}
+          <WinrateEloTable />
           {/* <TrophiesTable /> */}
         </View>
       </div>
