87503b4c0f0051f8b91a590924ec1aa0ffea6e80
Simon Cui <scui@asr.ocgov.com>
2024-09-04 16:38:52 -0700

    2x2 update

--- src/Pages/doublemasters.js
+++ src/Pages/doublemasters.js
@@ -11,10 +11,10 @@ const DoubleMasters = () => {
   return (
     <>
       <h1 style={{ textAlign: "center" }}>
-        Double Masters 2022 Stats (36 Drafts Total)
+        Double Masters 2022 Stats (37 Drafts Total)
       </h1>
       <h2 style={{ textAlign: "center" }}>
-        ELO and Winrates (Min 4 drafts). Last Updated 3/21/24
+        ELO and Winrates (Min 4 drafts). Last Updated 8/29/24
       </h2>
       <div>
         <View style={styles.container}>
