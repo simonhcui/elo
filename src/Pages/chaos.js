0e6b146491323ed729aa2d9c4793bf6240ee32bd
Simon Cui <scui@asr.ocgov.com>
2024-09-11 13:37:15 -0700

    chaos 9/7

--- src/Pages/chaos.js
+++ src/Pages/chaos.js
@@ -5,9 +5,9 @@ import { WinrateEloTable } from "../Components/Chaos/WinrateEloTable";
 const ModernHorizonsTwo = () => {
   return (
     <>
-      <h1 style={{ textAlign: "center" }}>Chaos Stats (60 Drafts Total)</h1>
+      <h1 style={{ textAlign: "center" }}>Chaos Stats (70 Drafts Total)</h1>
       <h2 style={{ textAlign: "center" }}>
-        ELO and Winrates (Min 4 drafts). Last Updated 8/1/24
+        ELO and Winrates (Min 4 drafts). Last Updated 9/7/24
       </h2>
       <h2 style={{ textAlign: "center" }}></h2>
       <div>
